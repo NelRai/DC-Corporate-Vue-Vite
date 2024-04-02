271f4347ac4e3e79619c2923ce782d48018af669
Nelson Rainsborough <nelson.rainsborough@gmail.com>
2024-04-02 17:53:16 +0200

    Moved  Header to compnent

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,10 @@ import { onMounted } from "vue";
 import { ref } from 'vue'
 
 //Components
-import HelloWorld from "./components/HelloWorld.vue";
-import WrapperContent from "./components/wrapper-content.vue";
-import navLink from "./components/nav-link.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+// import WrapperContent from "./components/wrapper-content.vue";
+import Header from "./components/Header.vue";
+// import navLink from "./components/nav-link.vue";
 import btnPrimary from "./components/btn-primary.vue";
 import btnSecondary from "./components/btn-secondary.vue";
 import headlineH2 from "./components/headline-h2.vue";
@@ -35,13 +36,13 @@ const visibleWorkshop = ref(false)
 
 //burger menu code
 
-window.addEventListener("DOMContentLoaded", (event) => {
-  const tham = document.querySelector(".tham");
+// window.addEventListener("DOMContentLoaded", (event) => {
+//   const tham = document.querySelector(".tham");
 
-  tham.addEventListener('click', () => {
-    tham.classList.toggle('tham-active');
-  });
-});
+//   tham.addEventListener('click', () => {
+//     tham.classList.toggle('tham-active');
+//   });
+// });
 
 const hamburger = ref(false)
 
@@ -64,62 +65,12 @@ window.onscroll = function () {
 
 <template>
   <div class="wrapper ">
-    <header id="navbar" class="fixed flex lg:grid px-4 lg:pt-3 lg:px-0 items-center lg:items-end w-full m-auto max-w-7xl left-1/2 z-99 navbar transition-all rounded-b-lg shadow-md backdrop-blur-sm bg-dc-neutral50/70 lg:bg-transparent lg:backdrop-blur-none lg:shadow-none"  :class="[ { navbarHidden: hamburger }]"    >
-      <!--change to fixed if on scroll header hide is implemented  -->
-      
-      <div class="flex-1 lg:absolute lg:top-0">
-        <div class="">
-          <img src="./assets/Logo_DEEP-CONTENT-Heise_micro.svg" alt=""
-            class="h-auto w-44 lg:w-52 lg:bg-dc-neutral50/80  py-4 lg:p-4 lg:rounded-b-lg lg:shadow-md lg:backdrop-blur-sm" />
-        </div>
-      </div>
-      <!-- Hamburger -->
-      <div class="tham tham-e-squeeze tham-w-6 lg:hidden z-99" @click="hamburger = !hamburger" :class="[ { test: hamburger }]" >
-        <div class="tham-box">
-          <div class="tham-inner" />
-        </div>
-      </div>
 
 
-      <nav class="hidden justify-self-center lg:flex bg-dc-neutral50/80 rounded-full flex-initial justify-center shadow-md backdrop-blur-sm  lg:visible">
-        <ul class="flex flex-col lg:h-auto lg:flex-row gap-5 py-3 px-8 justify-start items-start lg:justify-center lg:items-center z-99">
-          <li class="p-0 m-0 h-fit leading-4">
-            <navLink text="Über uns" link="#ueber-uns" />
-          </li>
-          <li class="p-0 m-0 h-fit leading-4">
-            <navLink text="KI-Agentur" link="#ki-beratung" />
-          </li>
-          <li class="p-0 m-0 h-fit leading-4">
-            <navLink text="Medienhaus" link="#medienhaus" />
-          </li>
-          <li class="p-0 m-0 h-fit leading-4">
-            <navLink text="Kontakt" link="#kontakt" />
-          </li>
-        </ul>
-      </nav>
-      <div class="flex-1 justify-center gap-2 text-lg font-normal hidden">
-        <span class="text-dc-400 underline">EN</span>
-        <span class="text-dc-neutral900">DE</span>
-      </div>
-    </header>
+    <Header />
 
-    <div class="hamburgerMenu fixed bg-dc-neutral50/90 flex-initial justify-center shadow-md backdrop-blur-sm lg:hidden z-70" :class="[ { hamburgerVisible: hamburger }]" >
- 
-        <ul class="flex flex-col    w-screen h-screen  gap-5 py-3 px-8 pt-32 justify-start items-center">
-          <li class="p-0 m-0 h-fit leading-4 ">
-            <navLink text="Über uns" link="#ueber-uns" class="!text-2xl"  @click="hamburger = !hamburger"/>
-          </li>
-          <li class="p-0 m-0 h-fit leading-4">
-            <navLink text="KI-Agentur" link="#ki-beratung" class="!text-2xl" @click="hamburger = !hamburger" />
-          </li>
-          <li class="p-0 m-0 h-fit leading-4">
-            <navLink text="Medienhaus" link="#medienhaus" class="!text-2xl"  @click="hamburger = !hamburger"/>
-          </li>
-          <li class="p-0 m-0 h-fit leading-4">
-            <navLink text="Kontakt" link="#kontakt" class="!text-2xl"  @click="hamburger = !hamburger"/>
-          </li>
-        </ul>
-      </div>
+   
+
 
     <div class="flex flex-col gap-8 pt-32 pb-28 lg:pt-44 lg:pb-28 lg:gap-32 ">
       <div class="flex flex-col gap-2 lg:gap-8 text-center lg:text-left w-full max-w-7xl m-auto">
---
+++ src/components/Header.vue
@@ -0,0 +1,72 @@
+<script setup>
+import { ref } from 'vue'
+import navLink from "./nav-link.vue";
+
+const hamburger = ref(false)
+
+defineProps({
+  msg: String,
+})
+
+</script>
+
+<template>
+      <header id="navbar" class="fixed flex lg:grid px-4 lg:pt-3 lg:px-0 items-center lg:items-end w-full m-auto max-w-7xl left-1/2 z-99 navbar transition-all rounded-b-lg shadow-md backdrop-blur-sm bg-dc-neutral50/70 lg:bg-transparent lg:backdrop-blur-none lg:shadow-none"  :class="[ { navbarHidden: hamburger }]"    >
+      
+      <div class="flex-1 lg:absolute lg:top-0">
+        <div class="">
+          <img src="../assets/Logo_DEEP-CONTENT-Heise_micro.svg" alt=""
+            class="h-auto w-44 lg:w-52 lg:bg-dc-neutral50/80  py-4 lg:p-4 lg:rounded-b-lg lg:shadow-md lg:backdrop-blur-sm" />
+        </div>
+      </div>
+      <!-- Hamburger -->
+      <div class="tham tham-e-squeeze tham-w-6 lg:hidden z-99" @click="hamburger = !hamburger" :class="[ { 'tham-active': hamburger }]" >
+        <div class="tham-box">
+          <div class="tham-inner" />
+        </div>
+      </div>
+
+
+      <nav class="hidden justify-self-center lg:flex bg-dc-neutral50/80 rounded-full flex-initial justify-center shadow-md backdrop-blur-sm  lg:visible">
+        <ul class="flex flex-col lg:h-auto lg:flex-row gap-5 py-3 px-8 justify-start items-start lg:justify-center lg:items-center z-99">
+          <li class="p-0 m-0 h-fit leading-4">
+            <navLink text="Über uns" link="#ueber-uns" />
+          </li>
+          <li class="p-0 m-0 h-fit leading-4">
+            <navLink text="KI-Agentur" link="#ki-beratung" />
+          </li>
+          <li class="p-0 m-0 h-fit leading-4">
+            <navLink text="Medienhaus" link="#medienhaus" />
+          </li>
+          <li class="p-0 m-0 h-fit leading-4">
+            <navLink text="Kontakt" link="#kontakt" />
+          </li>
+        </ul>
+      </nav>
+      <div class="flex-1 justify-center gap-2 text-lg font-normal hidden">
+        <span class="text-dc-400 underline">EN</span>
+        <span class="text-dc-neutral900">DE</span>
+      </div>
+    </header>
+
+
+
+    <div class="hamburgerMenu fixed bg-dc-neutral50/90 flex-initial justify-center shadow-md backdrop-blur-sm lg:hidden z-70" :class="[ { hamburgerVisible: hamburger }]" >
+ 
+        <ul class="flex flex-col    w-screen h-screen  gap-5 py-3 px-8 pt-32 justify-start items-center">
+          <li class="p-0 m-0 h-fit leading-4 ">
+            <navLink text="Über uns" link="#ueber-uns" class="!text-2xl"  @click="hamburger = !hamburger"/>
+          </li>
+          <li class="p-0 m-0 h-fit leading-4">
+            <navLink text="KI-Agentur" link="#ki-beratung" class="!text-2xl" @click="hamburger = !hamburger" />
+          </li>
+          <li class="p-0 m-0 h-fit leading-4">
+            <navLink text="Medienhaus" link="#medienhaus" class="!text-2xl"  @click="hamburger = !hamburger"/>
+          </li>
+          <li class="p-0 m-0 h-fit leading-4">
+            <navLink text="Kontakt" link="#kontakt" class="!text-2xl"  @click="hamburger = !hamburger"/>
+          </li>
+        </ul>
+      </div>
+
+</template>
